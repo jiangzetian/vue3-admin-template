fa69aca82e1bef75ee535ec80cc3976cb9e58991
蒋泽天 <1139246375@qq.com>
2021-11-02 14:42:37 +0800

    整理代码

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,21 +1,16 @@
 <template>
     <div>
-        <a-button @click="outLogin">退出登录</a-button>
         <h1>home</h1>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import homeHook from './moduls/index.ts';
 
 export default defineComponent({
     name: 'home',
     setup() {
-        const { outLogin } = homeHook;
-        return {
-            outLogin,
-        };
+        return {};
     },
 });
 </script>
